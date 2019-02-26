aa43e4e086a656c37e1bb62e1382d730e776952f
Vadim Ryazanov <vadimvoronezh@yandex.ru>
2019-02-26 19:58:15 +0300

    Handle http response different way

--- tests/MobileToken.js
+++ tests/MobileToken.js
@@ -6,12 +6,12 @@ describe('It tests MobileToken class', function() {
 
     const mobileToken = new MobileToken('+XXXXXXXXXXX')
 
-    mock.get('/mobile/token?phone=+XXXXXXXXXXX&code=incorrect-code').reply(304)
+    mock.get('/mobile/token?phone=+XXXXXXXXXXX&code=incorrect-code&noredirect=1').reply(503)
     it('checks `token` method raises exception if confirmation code incorrect', () => {
         chai.expect(mobileToken.token('incorrect-code')).to.be.eventually.rejectedWith(Error)
     })
 
-    mock.get('/mobile/token?phone=+XXXXXXXXXXX&code=correct-code').reply(200, 'real-token')
+    mock.get('/mobile/token?phone=+XXXXXXXXXXX&code=correct-code&noredirect=1').reply(200, 'real-token')
     it('check `token` method returns token if confirmation code is correct', () => {
         chai.assert.becomes(mobileToken.token('correct-code'), 'real-token')
     })
